28d3e15631f99c39ced92833f86df3986606e95e
Okyarilkay <107274565+Okyarilkay@users.noreply.github.com>
2022-09-29 15:49:30 +0300

    Add files via upload

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/DortIslem.java
@@ -0,0 +1,17 @@
+package reCapDemo_Classes;
+
+public class DortIslem {
+	public int Sum(int number1,int number2) {
+		return number1+number2;
+	}
+	public int Minus(int number1,int number2) {
+		return number1-number2;
+	}
+	public int Divide(int number1,int number2) {
+		return number1/number2;
+	}
+	public int Multiply(int number1,int number2) {
+		return number1*number2;
+		
+	}
+}
