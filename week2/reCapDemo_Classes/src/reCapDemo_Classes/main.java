2f791d1c502843bca3d717b383231380f0f1416f
Okyarilkay <107274565+Okyarilkay@users.noreply.github.com>
2022-10-09 18:23:04 +0300

    kodlama.io-week2

---
+++ week2/reCapDemo_Classes/src/reCapDemo_Classes/main.java
@@ -0,0 +1,12 @@
+package reCapDemo_Classes;
+
+public class main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.Sum(3, 4);
+		int sonuc2 = dortIslem.Divide(5, 2);
+		System.out.println(sonuc);
+	}
+
+}
