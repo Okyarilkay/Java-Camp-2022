e0bc890bc5cf374cb07bc512a0913e8b14a4565f
Okyarilkay <107274565+Okyarilkay@users.noreply.github.com>
2022-10-14 17:48:36 +0300

    a

--- week3/homework2/overriding/src/overriding/BaseKrediManager.java
+++
@@ -1,8 +0,0 @@
-package overriding;
-
-public class BaseKrediManager {
-	//final keyword ü override yapılmasını engeller:public final double hesapla(double tutar)
-	public double hesapla(double tutar) {
-		return tutar * 1.18;
-	}
-}
